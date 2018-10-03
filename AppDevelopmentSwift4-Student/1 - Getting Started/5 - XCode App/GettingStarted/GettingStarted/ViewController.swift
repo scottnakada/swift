367e45f28646d309e01bee543a1f3ddc26b6d470
Scott Nakada <scott.nakada@sony.com>
2018-10-02 20:57:11 -0700

    Add 1.5, and 1.8

---
+++ AppDevelopmentSwift4-Student/1 - Getting Started/5 - XCode App/GettingStarted/GettingStarted/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by Scott Nakada on 10/2/18.
+//  Copyright © 2018 Scott Nakada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        var names = ["Tammy", "Cole"]
+        names.removeFirst()
+        names.removeFirst()
+
+    }
+
+
+}
+
