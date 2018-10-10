9da4f5b80e9e1ad8d96ef039c9243ff2037a23bc
Scott Nakada <scott.nakada@sony.com>
2018-10-09 21:36:41 -0700

    AutoLayoutPractice

---
+++ AppDevelopmentSwift4-Student/2 - Introduction to UIKit/8 - Projects/AutoLayoutPractice/AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by Scott Nakada on 10/8/18.
+//  Copyright © 2018 Scott Nakada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
