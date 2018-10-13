263fd50ed9f2ec8d6234385701cceebaa9ca9b2b
Scott Nakada <scott.nakada@sony.com>
2018-10-12 19:59:59 -0700

    ApplePie Part 2

---
+++ AppDevelopmentSwift4-Student/2 - Introduction to UIKit/8 - Projects/Apple Pie/Apple Pie/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Scott Nakada on 10/12/18.
+//  Copyright © 2018 Scott Nakada. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
--- AppDevelopmentSwift4-Student/2 - Introduction to UIKit/8 - Projects/Apple Pie/Apple Pie/ViewController.swift
+++ AppDevelopmentSwift4-Student/2 - Introduction to UIKit/8 - Projects/Apple Pie/Apple Pie/ViewController.swift
@@ -9,14 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    /* Outlets */
     @IBOutlet weak var treeImageView: UIImageView!
     @IBOutlet weak var correctWordLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var letterButtons: [UIButton]!
     
+    /* Constants and Variables */
+    var listOfWords = ["apple", "orange", "banana", "peach", "pineapple", "grapes", "kiwi", "watermelon", "honeydew"]
+    let incorrectMovesAllowed = 7   /* Max number of incorrect words allowed */
+    var totalWins = 0               /* Total wins so far */
+    var totalLosses = 0             /* Total losses so far */
+    var currentGame: Game!          /* Pointer to the current game */
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        /* Start a new round when the game loads */
+        newRound()
+    }
+    
+    /* Play a new round of the game */
+    func newRound() {
+        /* Get the next word from the front of the list of words */
+        let newWord = listOfWords.removeFirst()
+        /* instantiate a new Game as the current game */
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+    }
+    
+    /* Update the user interface() */
+    func updateUI() {
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
